a1e6b910a627bb7c07e64d4dfe6decceccd3cd7b
Saygachenko <curatoroff@gmail.com>
2023-11-06 15:14:03 +0200

    Init

---
+++ Source/MazeOfNightmares/MazeOfNightmares.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MazeOfNightmares : ModuleRules
+{
+	public MazeOfNightmares(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ Source/MazeOfNightmares/MazeOfNightmaresGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MazeOfNightmaresGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMazeOfNightmaresGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMazeOfNightmaresGameMode();
+};
+
+
+
